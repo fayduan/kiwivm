9de0cda628810d5ed1b2db75be0d43dc4cfd7b75
DUAN Yufei <duanyufeichn@gmail.com>
2017-03-13 15:52:00 +0800

    添加：获取动态信息

--- app/src/main/java/cn/duanyufei/kiwivm/MainActivity.java
+++ app/src/main/java/cn/duanyufei/kiwivm/MainActivity.java
@@ -2,20 +2,33 @@ package cn.duanyufei.kiwivm;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import cn.duanyufei.kiwivm.model.LiveServiceInfo;
+import cn.duanyufei.kiwivm.model.VzStatus;
 
 public class MainActivity extends AppCompatActivity {
 
     final static String TAG = "MainActivity";
-    
+
+    private TextView tvStatus;
+    private TextView tvIpv4;
+    private TextView tvIpv6;
+    private TextView tvData;
+    private TextView tvDate;
+    private FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,33 +36,66 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        tvStatus = (TextView) findViewById(R.id.info_status);
+        tvIpv4 = (TextView) findViewById(R.id.info_ipv4);
+        tvIpv6 = (TextView) findViewById(R.id.info_ipv6);
+        tvData = (TextView) findViewById(R.id.info_data_counter);
+        tvDate = (TextView) findViewById(R.id.info_data_next_reset);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                getLiveInfo();
+                Toast.makeText(MainActivity.this, R.string.refresh, Toast.LENGTH_SHORT).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
 
-        init();
+        getLiveInfo();
     }
 
-    private void init() {
+    private void getLiveInfo() {
+
+        fab.setClickable(false);
+
         Log.i(TAG, "init: ");
         NetUtil.getInstance(this).getMessage(AppDefine.SERVER + AppDefine.LIVE_SERVICE_INFO + AppDefine.ID,
                 new NetUtil.MOkCallBack() {
 
                     @Override
                     public void onSuccess(String rep) {
-                        LiveServiceInfo info = (LiveServiceInfo)GsonUtil.fromJson(rep, LiveServiceInfo.class);
+                        LiveServiceInfo info = (LiveServiceInfo) GsonUtil.fromJson(rep, LiveServiceInfo.class);
+                        updateTextView(info);
+                        fab.setClickable(true);
                     }
 
                     @Override
                     public void onError() {
-
+                        fab.setClickable(true);
                     }
                 });
+    }
+
+    private void updateTextView(LiveServiceInfo info) {
+        try {
+            VzStatus status = info.getVz_status();
+            tvStatus.setText(status.getStatus());
+            tvIpv4.setText(info.getIp_addresses().get(0));
+            tvIpv6.setText(info.getIp_addresses().get(1));
+            long data = info.getData_counter();
+            double doubleData = (double) data / 1024.0 / 1024.0 / 1024.0;
+            String dispData = String.format("%.8f GB", doubleData);
+            tvData.setText(dispData);
+            Date time = new Date(info.getData_next_reset() * 1000);
+            DateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+            String dispDate = String.format(format.format(time));
+            tvDate.setText(dispDate);
+        } catch (Exception e) {
+
+        }
+
     }
 
     @Override
--- app/src/main/java/cn/duanyufei/kiwivm/model/VzStatus.java
+++ app/src/main/java/cn/duanyufei/kiwivm/model/VzStatus.java
@@ -24,6 +24,158 @@ public class VzStatus {
     private String swappages_b;
     private String physpages;
     private String physpages_l;
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public String getKmemsize() {
+        return kmemsize;
+    }
+
+    public void setKmemsize(String kmemsize) {
+        this.kmemsize = kmemsize;
+    }
+
+    public String getKmemsize_b() {
+        return kmemsize_b;
+    }
+
+    public void setKmemsize_b(String kmemsize_b) {
+        this.kmemsize_b = kmemsize_b;
+    }
+
+    public String getLoad_average() {
+        return load_average;
+    }
+
+    public void setLoad_average(String load_average) {
+        this.load_average = load_average;
+    }
+
+    public String getNproc() {
+        return nproc;
+    }
+
+    public void setNproc(String nproc) {
+        this.nproc = nproc;
+    }
+
+    public String getNproc_b() {
+        return nproc_b;
+    }
+
+    public void setNproc_b(String nproc_b) {
+        this.nproc_b = nproc_b;
+    }
+
+    public String getNumfile() {
+        return numfile;
+    }
+
+    public void setNumfile(String numfile) {
+        this.numfile = numfile;
+    }
+
+    public String getNumfile_b() {
+        return numfile_b;
+    }
+
+    public void setNumfile_b(String numfile_b) {
+        this.numfile_b = numfile_b;
+    }
+
+    public String getNumtcpsock() {
+        return numtcpsock;
+    }
+
+    public void setNumtcpsock(String numtcpsock) {
+        this.numtcpsock = numtcpsock;
+    }
+
+    public String getNumtcpsock_b() {
+        return numtcpsock_b;
+    }
+
+    public void setNumtcpsock_b(String numtcpsock_b) {
+        this.numtcpsock_b = numtcpsock_b;
+    }
+
+    public String getOomguarpages() {
+        return oomguarpages;
+    }
+
+    public void setOomguarpages(String oomguarpages) {
+        this.oomguarpages = oomguarpages;
+    }
+
+    public String getOomguarpages_b() {
+        return oomguarpages_b;
+    }
+
+    public void setOomguarpages_b(String oomguarpages_b) {
+        this.oomguarpages_b = oomguarpages_b;
+    }
+
+    public String getPhyspages() {
+        return physpages;
+    }
+
+    public void setPhyspages(String physpages) {
+        this.physpages = physpages;
+    }
+
+    public String getPhyspages_l() {
+        return physpages_l;
+    }
+
+    public void setPhyspages_l(String physpages_l) {
+        this.physpages_l = physpages_l;
+    }
+
+    public String getPrivvmpages() {
+        return privvmpages;
+    }
+
+    public void setPrivvmpages(String privvmpages) {
+        this.privvmpages = privvmpages;
+    }
+
+    public String getPrivvmpages_b() {
+        return privvmpages_b;
+    }
+
+    public void setPrivvmpages_b(String privvmpages_b) {
+        this.privvmpages_b = privvmpages_b;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSwappages() {
+        return swappages;
+    }
+
+    public void setSwappages(String swappages) {
+        this.swappages = swappages;
+    }
+
+    public String getSwappages_b() {
+        return swappages_b;
+    }
+
+    public void setSwappages_b(String swappages_b) {
+        this.swappages_b = swappages_b;
+    }
 }
 //      "status":"running",
 //      "hostname":"localhost.localdomain",
